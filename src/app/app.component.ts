3e2a4ee9a93f07a399f159d4fedc4dda26116b51
Nikita Jani <nikitanehaj492@gmail.com>
2024-08-09 00:18:41 +0530

    feat: displayling 1k nodes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import ForceGraph from 'force-graph';
+import { graphData } from './data';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'network-graph-poc';
+export class AppComponent implements OnInit {
+  public ngOnInit(): void {
+    const myGraph = ForceGraph();
+    myGraph(document.getElementById('graph') as HTMLElement)
+      .backgroundColor('#101020')
+      .nodeRelSize(6)
+      .linkColor(() => 'rgba(255,255,255,0.2)')
+      .linkDirectionalParticles(1)
+      .nodeAutoColorBy('user')
+      .nodeLabel((node) => `${node.id}`)
+      .graphData(graphData);
+  }
 }
